6b9064898320b9b63e9e23bd8b14b4833c032d7a
Rookoo <sebastiansroka91@gmail.com>
2018-01-09 14:31:27 +0100

    modal box - js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,49 @@
 (function($) {
     $(window).load(function() { 
-  $('#status').fadeOut(); 
-  $('#preload').delay(350).fadeOut('slow'); 
-  $('body').delay(350).css({'overflow':'visible'});
+       $('#status').fadeOut(); 
+         $('#preload').delay(350).fadeOut('slow'); 
+           $('body').delay(350).css({'overflow':'visible'});
     });
+
 })(jQuery)
 
 $(document).ready(function() {
 
             $('#fullpage').fullpage({
                 anchors: ['Dj Tomek', 'Oferta', 'O mnie', 'Referencje', 'FAQ', 'Kontakt'],
-                navigation: true,
-                slidesNavigation: true,
-                navigationPosition: 'right',
-                navigationTooltips: ['Dj Tomek', 'Oferta', 'O mnie', 'Referencje', 'FAQ', 'Kontakt'],
-                responsiveWidth: 1023,
-                afterResponsive: function(isResponsive){  
-                }
-
+                  navigation: true,
+                    slidesNavigation: true,
+                      navigationPosition: 'right',
+                        navigationTooltips: ['Dj Tomek', 'Oferta', 'O mnie', 'Referencje', 'FAQ', 'Kontakt'],
+                          responsiveWidth: 1023,
             });
 
-            var $toggleButton = $('#nav-icon1'),
-                $menuWrap = $('.menu-wrap'),
-                $scrollingDiv = $('#nav-icon1'),
-                $slideArrowPrev = $('.slidesjs-previous'),
-                $slideArrowNext = $('.slidesjs-next'),
-                $bgArrowSlidePrev = $('.background-slidejs-prev'),
-                $bgArrowSlideNext = $('.background-slidejs-next');
-
+            var $toggleButton = $('#nav-icon'),
+                  $menuWrap = $('.menu-wrap'),
+                    $scrollingDiv = $('#nav-icon'),
+                      $slideArrowPrev = $('.slidesjs-previous'),
+                        $slideArrowNext = $('.slidesjs-next'),
+                          $bgArrowSlidePrev = $('.background-slidejs-prev'),
+                            $bgArrowSlideNext = $('.background-slidejs-next');
+                              // $modalparent = $('.modal')
+                              //   $modalBtn = $('.img-offert')
+                              //     $spanClose = $('.modal-close')
 
 
    $toggleButton.on('click', function() {
         $(this).toggleClass('open');
-        $menuWrap.toggleClass('menu-show');
+            $menuWrap.toggleClass('menu-show');
     });
 
 
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    if(scroll >= 450) {
-        $('.hamburger').attr('style', 'background: #000000');
+      if(scroll >= 450) {
+        // $('.hamburger').attr('style', 'background: #000000');
+        $('.hamburger').addClass('hamburger-black');
     } else {
-        $('.hamburger').removeAttr('style', 'background: #000000');
+        // $('.hamburger').removeAttr('style', 'background: #000000');
+        $('.hamburger').removeClass('hamburger-black');
     }
 });
 
@@ -55,7 +57,7 @@ $(window).scroll(function() {
       $('#slides2').slidesjs({
         width: 940,
         height: 528,
-        navigation: false,
+        navigation: false
       });
 
       $('#slides3').slidesjs({
@@ -93,7 +95,7 @@ $slideArrowNext.hover(function(){
 
     function close_accordion_section() {
         $('.accordion .accordion-section-title').removeClass('active');
-        $('.accordion .panel').slideUp(100).removeClass('open');
+          $('.accordion .panel').slideUp(100).removeClass('open');
     }
 
   $('.accordion-section-title').click(function(e) {
@@ -109,5 +111,53 @@ $slideArrowNext.hover(function(){
  
         e.preventDefault();
     });
+
+      
+      var modalparent = document.getElementsByClassName("modal_multi");
+        var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+        var span_close_multi = document.getElementsByClassName("modal-close");
+
+        // When the user clicks the button, open the modal
+        function setDataIndex() {
+
+            for (i = 0; i < modal_btn_multi.length; i++)
+            {
+                modal_btn_multi[i].setAttribute('data-index', i);
+                modalparent[i].setAttribute('data-index', i);
+                span_close_multi[i].setAttribute('data-index', i);
+            }
+        }
+
+
+        for (i = 0; i < modal_btn_multi.length; i++)
+        {
+            modal_btn_multi[i].onclick = function() {
+                var ElementIndex = this.getAttribute('data-index');
+                modalparent[ElementIndex].style.visibility = "visible";
+                modalparent[ElementIndex].style.opacity = "1";
+                modalparent[ElementIndex].style.transition = "opacity 0.25s linear";
+            };
+
+            span_close_multi[i].onclick = function() {
+                var ElementIndex = this.getAttribute('data-index');
+                modalparent[ElementIndex].style.visibility = "hidden";
+                modalparent[ElementIndex].style.opacity = "0";
+                modalparent[ElementIndex].style.transition = "visibility 0.25s linear 0.25s,opacity 0.25s linear";
+            };
+
+        }
+
+        window.onload = function() {
+
+            setDataIndex();
+        };
+
+        window.onclick = function(event) {
+            if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+                modalparent[event.target.getAttribute('data-index')].style.visibility = "hidden";
+                modalparent[event.target.getAttribute('data-index')].style.opacity = "0";
+                modalparent[event.target.getAttribute('data-index')].style.transition = "visibility 0.25s linear 0.25s,opacity 0.25s linear";
+            }
+        };
 
 });
